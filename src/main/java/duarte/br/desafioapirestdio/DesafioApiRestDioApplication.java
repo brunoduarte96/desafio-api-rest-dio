f6fed72d50b6d52cf1a2b7ff7eb0ed42fc040734
brunoduarte96 <brunoduarte96@outlook.com>
2023-10-18 20:44:43 -0300

    Initial commit

---
+++ src/main/java/duarte/br/desafioapirestdio/DesafioApiRestDioApplication.java
@@ -0,0 +1,13 @@
+package duarte.br.desafioapirestdio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioApiRestDioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioApiRestDioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/duarte/br/desafioapirestdio/DesafioApiRestDioApplicationTests.java
@@ -0,0 +1,13 @@
+package duarte.br.desafioapirestdio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioApiRestDioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
