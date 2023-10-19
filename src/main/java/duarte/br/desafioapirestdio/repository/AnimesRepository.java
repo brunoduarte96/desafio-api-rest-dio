5070fcb8bf3f6b9f6a47e68e229966ec177cfc35
brunoduarte96 <brunoduarte96@outlook.com>
2023-10-19 00:39:14 -0300

    Correção na busca do findByTitle

--- src/main/java/duarte/br/desafioapirestdio/repository/AnimesRepository.java
+++ src/main/java/duarte/br/desafioapirestdio/repository/AnimesRepository.java
@@ -3,5 +3,8 @@ package duarte.br.desafioapirestdio.repository;
 import duarte.br.desafioapirestdio.model.Animes;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AnimesRepository extends JpaRepository<Animes, Long> {
+    List<Animes> findByTitle(String title);
 }
