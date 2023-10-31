efffd0c4249a7aac5ac55e46f4d945ffaceceda7
brunoduarte96 <brunoduarte96@outlook.com>
2023-10-31 20:23:51 -0300

    Criação entidade Director

---
+++ src/main/java/duarte/br/desafioapirestdio/model/Director.java
@@ -0,0 +1,23 @@
+package duarte.br.desafioapirestdio.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Director {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @ManyToMany
+    private List<Animes>anime;
+}
