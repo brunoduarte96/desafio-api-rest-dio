36cec80a29dceef79fbf0acbb78fe353d761f9a7
brunoduarte96 <brunoduarte96@outlook.com>
2023-10-31 20:39:39 -0300

    Criação StudioRepository

---
+++ src/main/java/duarte/br/desafioapirestdio/repository/StudioRepository.java
@@ -0,0 +1,7 @@
+package duarte.br.desafioapirestdio.repository;
+
+import duarte.br.desafioapirestdio.model.Studio;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudioRepository extends JpaRepository<Studio, Long> {
+}
