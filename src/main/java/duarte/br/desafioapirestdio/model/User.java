4c4e2db707f0d7ebef49a83046f3c89fb8480a4e
brunoduarte96 <brunoduarte96@outlook.com>
2023-10-31 20:35:16 -0300

    Alteração name

--- src/main/java/duarte/br/desafioapirestdio/model/User.java
+++ src/main/java/duarte/br/desafioapirestdio/model/User.java
@@ -11,7 +11,7 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-public class Usuario {
+public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
