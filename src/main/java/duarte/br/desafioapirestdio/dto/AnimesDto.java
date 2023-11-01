e6bb88f8415bcb1e970b396d89e2be957a5ae02e
brunoduarte96 <brunoduarte96@outlook.com>
2023-10-31 21:22:16 -0300

    Criação AnimesDto

---
+++ src/main/java/duarte/br/desafioapirestdio/dto/AnimesDto.java
@@ -0,0 +1,31 @@
+package duarte.br.desafioapirestdio.dto;
+
+import duarte.br.desafioapirestdio.model.Animes;
+import duarte.br.desafioapirestdio.model.Director;
+import duarte.br.desafioapirestdio.model.Studio;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AnimesDto {
+    private Long id;
+    private String title;
+    private String description;
+    private String imageUrl;
+    private List<Director> directors;
+    private List<Studio> studios;
+
+    public AnimesDto(Animes entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.description = entity.getDescription();
+        this.imageUrl = entity.getImageUrl();
+        this.directors = entity.getDirectors();
+        this.studios = entity.getStudios();
+    }
+}
