9224595fc132286814523bf118a1a768b65ed42c
brunoduarte96 <brunoduarte96@outlook.com>
2023-11-23 19:57:40 -0300

    Modificações na entidade Animes

--- src/main/java/duarte/br/desafioapirestdio/model/Animes.java
+++ src/main/java/duarte/br/desafioapirestdio/model/Animes.java
@@ -1,11 +1,13 @@
 package duarte.br.desafioapirestdio.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Data
@@ -17,23 +19,24 @@ public class Animes {
     private Long id;
     private String title;
 
-    @ManyToMany
+    @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(
             name = "anime_studio",
             joinColumns = @JoinColumn(name = "anime_id"),
             inverseJoinColumns = @JoinColumn(name = "studio_id")
     )
-    private List<Studio> studios;
+    private Set<Studio> studios = new HashSet<>();
 
 
-    @ManyToMany
+    @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(
             name = "anime_director",
             joinColumns = @JoinColumn(name = "anime_id"),
             inverseJoinColumns = @JoinColumn(name = "director_id")
     )
-    private List<Director> directors;
+    private Set<Director>directors = new HashSet<>();
 
     private String description;
     private String imageUrl;
+
 }
